4a4a22f6c779be3ef8f183726d1774d8a5979509
Geoff Taylor <kidunot89@gmail.com>
2019-12-31 14:56:17 -0500

    tests updated

--- src/utils/test-response.js
+++ src/utils/test-response.js
@@ -1,7 +1,32 @@
 import { expect } from 'chai';
+import { find } from 'lodash';
 
 const testResponse = (response, request, index) => {
+  request.actions.forEach(({ type, variables }) => {
+  
+    switch (type) {
+      case 'addToCart':
+        break;
+      case 'updateItemQuantities':
+        break;
+      case 'removeItemsFromCart':
+        break;
+      case 'restoreCartItems':
+        break;
+      case 'emptyCart':
+        break;
+      case 'applyCoupon':
+        break;
+      case 'removeCoupons':
+        break;
+      default:
+        break;
+    }
+  });
+  
+    console.log({ response, request, index });
     
+
 };
 
 export default testResponse;
